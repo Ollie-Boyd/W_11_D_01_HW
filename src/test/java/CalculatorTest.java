1bafa60ea4887e7e9e4756eb91b20277f67cbbe9
Ollie Boyd <ollyboyd@hotmail.co.uk>
2020-07-21 09:15:51 +0100

    finished mvp

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,33 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
+    private Calculator calculator;
+
+    @Before
+    public void before(){
+        this.calculator = new Calculator();
+    }
+
+    @Test
+    public void add(){
+        assertEquals(11,this.calculator.add(5,6));
+    }
+
+    @Test
+    public void subtract(){
+        assertEquals(5,calculator.subtract(11,6));
+    }
+
+    @Test
+    public void multiply(){
+        assertEquals(24, calculator.multiply(4, 6));
+    }
+
+    @Test
+    public void divide(){
+        assertEquals(3.3333333333333335, calculator.divide(10.00000000, 3), 0.01);
+    }
 }
--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -1,2 +1,38 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class WaterBottleTest {
+
+    private WaterBottle waterBottle;
+
+    @Before
+    public void before(){
+        this.waterBottle = new WaterBottle(100);
+    }
+
+    @Test
+    public void hasVolume(){
+        assertEquals(100,waterBottle.getVolume());
+    }
+
+    @Test
+    public void takeDrink(){
+        waterBottle.takeDrink();
+        assertEquals(90,waterBottle.getVolume());
+    }
+
+    @Test
+    public void empty(){
+        waterBottle.empty();
+        assertEquals(0,waterBottle.getVolume());
+    }
+
+    @Test
+    public void fill(){
+        waterBottle.fill();
+        assertEquals(100,waterBottle.getVolume());
+    }
 }
+
